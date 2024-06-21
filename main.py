f69cee498cd2adea321b42608d9c52b63590dcb3
ChanWoo Kim <71691159+kCW-tb@users.noreply.github.com>
2024-06-21 10:46:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -34,15 +34,15 @@ def check_distance(hand_landmarks):
     # 12번 노드(x, y) 좌표
     x12, y12 = hand_landmarks.landmark[12].x, hand_landmarks.landmark[12].y
 
-    # 두 점 사이의 거리 계산
+    # 두 노드 사이의 거리 계산
     distance = math.sqrt((x12 - x8)**2 + (y12 - y8)**2)
     distance = distance * 100
     return distance
 def decrease_current_stock(n):
-    stocks = db.stock_collection.find()  # 모든 데이터 가져오기
+    stocks = db.stock_collection.find()  
     data_to_update = None
     for idx, stock in enumerate(stocks):
-        if idx == n-1:  # 0부터 시작하는 인덱스를 고려하여 n-1로 체크
+        if idx == n-1:  # 인덱스를 고려하여 n-1로 체크
             current_stock = stock["Current_Stock"]
             new_current_stock = current_stock - 1
             data_to_update = stock
@@ -50,7 +50,6 @@ def decrease_current_stock(n):
             break
 
     if data_to_update:
-        # 데이터 업데이트
         db.stock_collection.update_one({"_id": data_to_update["_id"]}, {"$set": {"Current_Stock": data_to_update["Current_Stock"]}})
         print(f"{n}번 재고 하나 소진.")
     else:
@@ -63,9 +62,7 @@ def show_stock_data():
 db.stock_collection.delete_many({})
 product_number = -1
 
-# 데이터가 이미 존재하는지 확인
 if db.stock_collection.count_documents({}) == 0:
-    # 6행 3열의 데이터 생성
     stock_data = [
         {"Total_Stock": 10, "Current_Stock": 10, "Increase": 0},
         {"Total_Stock": 10, "Current_Stock": 10, "Increase": 0},
@@ -74,9 +71,7 @@ if db.stock_collection.count_documents({}) == 0:
         {"Total_Stock": 10, "Current_Stock": 10, "Increase": 0},
         {"Total_Stock": 10, "Current_Stock": 10, "Increase": 0}
     ]
-    # 데이터를 MongoDB에 삽입
     result = db.stock_collection.insert_many(stock_data)
-    # 삽입 결과 확인
     print(result)
 else:
     print("데이터가 이미 존재합니다. 삽입을 수행하지 않습니다.")
@@ -114,12 +109,12 @@ background[320:520, 300:500] = produrt_img4.copy()
 background[320:520, 550:750] = produrt_img5.copy()
 background[320:520, 800:1000] = produrt_img6.copy()
 
-# 미디어파이프를 이용하여 Hand 모델을 로드합니다.
+# load hand model
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands()
 mp_drawing = mp.solutions.drawing_utils # drawing_utils를 이용하여 랜드마크 드로잉
 
-# 비디오 스트림을 캡처합니다.
+#카메라에 대한 호출
 cap = cv2.VideoCapture(0)
 
 last_update_time = time.time()
@@ -129,19 +124,17 @@ while cap.isOpened():
     if current_time - last_update_time >= 70:  # 70초마다 업데이트
         print("재고 주문 전 현 상황")
         show_stock_data()
-        update_stock_values()  # 주식 가격을 업데이트합니다.
+        update_stock_values() #재고 업데이트
         print("재고 주문 후 현 상황")
         show_stock_data()
-        last_update_time = current_time  # 마지막 업데이트 시간을 갱신합니다.
+        last_update_time = current_time  #시간을 갱신합니다.
         
-
-    # 프레임을 읽어옵니다.
     success, image = cap.read()
     if not success:
         continue
     image = cv2.resize(image, (1280, 720))
 
-    # 미디어파이프에 손 이미지를 전달하여 스켈레톤을 추출합니다.
+    # 스켈레톤을 추출
     results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     
     image = background[:,:].copy()
@@ -149,13 +142,13 @@ while cap.isOpened():
     # 손이 검출된 경우
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
-            # 스켈레톤 점을 연결하여 표시합니다.
+            # 노드끼리의 점을 연결하여 표시
             mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
             break
         distance = check_distance(hand_landmarks)
         cv2.putText(image, "Distance : " + str(distance), (50, 600), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-        if distance < 10:
+        if distance < 10:    #거리가 10 미만일 떄 (검지와 중지가 겹쳐있을 때)
             x = int(hand_landmarks.landmark[8].x * 1280)
             y = int(hand_landmarks.landmark[8].y * 720)
             print("x : ", str(x) ,"y : "+ str(y))
@@ -190,13 +183,11 @@ while cap.isOpened():
             print(product_number)
 
     
-    # 'resized' 화면에 출력합니다.
+    # 화면에 출력
     cv2.imshow('Hand Tracking', image)
 
-    # 'q'를 누르면 종료합니다.
+    # 'q'를 누르면 종료
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-# 리소스를 해제합니다.
 cap.release()
 cv2.destroyAllWindows()
